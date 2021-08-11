5f4b402bcb3702d9c7bf54843e21531831c1bf6b
Siddharth <siddharth.muscat@gmail.com>
2021-08-11 19:44:07 +0530

    Add syntax higlighting

--- js/codeview.js
+++ js/codeview.js
@@ -1,11 +1,15 @@
-class CodeView {
+import {highlight} from './highlight.js';
+
+export default class CodeView {
 	constructor(element, initialCode) {
 		this.element = document.querySelector(element);
 		this.code = initialCode;
 		this.render();
+		// TODO option
+		this.language = 'javascript';
 	}
 
 	render() {
-		this.element.innerHTML = `<pre><code>${this.code}</code></pre>`
+		this.element.innerHTML = `<pre><code>${highlight(this.code, this.language)}</code></pre>`
 	}
 }
