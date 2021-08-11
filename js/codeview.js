88574b93d77fb19e64340ee106b2e62a76f8a4e7
Siddharth <siddharth.muscat@gmail.com>
2021-08-11 21:11:18 +0530

    Make opacity work

--- js/codeview.js
+++ js/codeview.js
@@ -8,9 +8,13 @@ export default class CodeView {
 		// Todo make language settable
 	}
 
-	render() {
-		this.element.innerHTML = `<pre><code>${
-			highlight(this.code).map(line => `<div>${line}</div>`).join('')
-		}</code></pre>`
+	render(options = {}) {
+		this.element.innerHTML = `<pre><code>${this._highlight(this.code, options)}</code></pre>`
+	}
+
+	_highlight(code, options) {
+		return highlight(code).map((line, index) => `<div style='opacity: ${
+			(options.range || []).includes(index + 1) ? 1 : 0.3
+		}'>${line}</div>`).join('')
 	}
 }
